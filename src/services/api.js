5438aff804818bebde23d921ee9e4b97b7774a61
HebleV <visheble@yahoo.com>
2020-06-23 23:52:54 +0530

    Added build changes

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const getStory = async (currentPageCount) => {
     //API
-    const url = `https://hn.algolia.com/api/v1/search?query=story&page=${currentPageCount}&hitsPerPage=20`;
+    const url = `https://hn.algolia.com/api/v1/search?query=story&page=${currentPageCount}&hitsPerPage=10`;
     try {
         const response = await axios.get(url);
         const data = await response.data;
